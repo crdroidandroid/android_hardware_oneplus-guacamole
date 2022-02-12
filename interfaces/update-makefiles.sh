e18deab74319edd8f949a8c92c585011ce8af39c
Lup Gabriel <gwolf2u.website@gmail.com>
2022-02-12 10:22:14 +0200

    hw-op: change to new path and cleanup unused stuff

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.oneplus:hardware/oneplus/interfaces"
+  "vendor.oneplus:hardware/oneplus-guacamole/interfaces"
